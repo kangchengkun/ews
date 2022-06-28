7806af1eb8a81f5505c89b1e232cf30a7c8405f6
kangchengkun <460876880@qq.com>
2022-06-28 16:37:55 +0800

    add go.mod file and update ews.

--- createitem.go
+++ createitem.go
@@ -8,64 +8,70 @@ import (
 // https://msdn.microsoft.com/en-us/library/office/aa563009(v=exchg.140).aspx
 
 type CreateItem struct {
-	XMLName		struct{}	`xml:"m:CreateItem"`
-	MessageDisposition		string	`xml:"MessageDisposition,attr"`
-	SavedItemFolderId	SavedItemFolderId	`xml:"m:SavedItemFolderId"`
-	Items			Messages		`xml:"m:Items"`
+	XMLName            struct{}          `xml:"m:CreateItem"`
+	MessageDisposition string            `xml:"MessageDisposition,attr"`
+	SavedItemFolderId  SavedItemFolderId `xml:"m:SavedItemFolderId"`
+	Items              Messages          `xml:"m:Items"`
 }
 
 type Messages struct {
-	Message		[]Message		`xml:"t:Message"`
+	Message []Message `xml:"t:Message"`
 }
 
 type SavedItemFolderId struct {
-	DistinguishedFolderId	DistinguishedFolderId	`xml:"t:DistinguishedFolderId"`
+	DistinguishedFolderId DistinguishedFolderId `xml:"t:DistinguishedFolderId"`
 }
 
 type DistinguishedFolderId struct {
-	Id		string		`xml:"Id,attr"`
+	Id string `xml:"Id,attr"`
 }
 
 type Message struct {
-	ItemClass		string		`xml:"t:ItemClass"`
-	Subject		string		`xml:"t:Subject"`
-	Body			Body			`xml:"t:Body"`
-	Sender		OneMailbox	`xml:"t:Sender"`
-	ToRecipients	XMailbox		`xml:"t:ToRecipients"`
+	ItemClass    string     `xml:"t:ItemClass"`
+	Subject      string     `xml:"t:Subject"`
+	Body         Body       `xml:"t:Body"`
+	Sender       OneMailbox `xml:"t:Sender"`
+	ToRecipients XMailbox   `xml:"t:ToRecipients"`
+	CcRecipients XMailbox   `xml:"t:CcRecipients"`
 }
 
 type Body struct {
-	BodyType		string	`xml:"BodyType,attr"`
-	Body			[]byte	`xml:",chardata"`
+	BodyType string `xml:"BodyType,attr"`
+	Body     []byte `xml:",chardata"`
 }
 
 type OneMailbox struct {
-	Mailbox		Mailbox			`xml:"t:Mailbox"`
+	Mailbox Mailbox `xml:"t:Mailbox"`
 }
 
 type XMailbox struct {
-	Mailbox		[]Mailbox			`xml:"t:Mailbox"`
+	Mailbox []Mailbox `xml:"t:Mailbox"`
 }
 
 type Mailbox struct {
-	EmailAddress		string		`xml:"t:EmailAddress"`
+	EmailAddress string `xml:"t:EmailAddress"`
 }
 
-func BuildTextEmail(from string, to []string, subject string, body []byte) ([]byte, error) {
+func BuildTextEmail(from string, to []string, cc []string, subject string, body []byte) ([]byte, error) {
 	c := new(CreateItem)
 	c.MessageDisposition = "SendAndSaveCopy"
 	c.SavedItemFolderId.DistinguishedFolderId.Id = "sentitems"
 	m := new(Message)
 	m.ItemClass = "IPM.Note"
 	m.Subject = subject
-	m.Body.BodyType = "Text"
+	m.Body.BodyType = "HTML"
 	m.Body.Body = body
 	m.Sender.Mailbox.EmailAddress = from
 	mb := make([]Mailbox, len(to))
+	mc := make([]Mailbox, len(cc))
 	for i, addr := range to {
 		mb[i].EmailAddress = addr
 	}
+	for i, addr := range cc {
+		mc[i].EmailAddress = addr
+	}
 	m.ToRecipients.Mailbox = append(m.ToRecipients.Mailbox, mb...)
+	m.CcRecipients.Mailbox = append(m.CcRecipients.Mailbox, mc...)
 	c.Items.Message = append(c.Items.Message, *m)
 	return xml.MarshalIndent(c, "", "  ")
 }
--- ews.go
+++ ews.go
@@ -1,16 +1,25 @@
-// 26 august 2016
 package ews
 
 import (
 	"bytes"
+	"crypto/tls"
+	"errors"
+	"fmt"
 	"net/http"
+	"regexp"
+	"strings"
+	"time"
+
+	httpNtlm "github.com/vadimi/go-http-ntlm"
 )
 
-// https://msdn.microsoft.com/en-us/library/office/dd877045(v=exchg.140).aspx
-// https://arvinddangra.wordpress.com/2011/09/29/send-email-using-exchange-smtp-and-ews-exchange-web-service/
-// https://msdn.microsoft.com/en-us/library/office/dn789003(v=exchg.150).aspx
+var (
+	UserName           string // mail or domain\account format
+	AccessToken        string
+	ExchangeServerAddr string
+)
 
-var soapheader = `<?xml version="1.0" encoding="utf-8" ?>
+var soapHeader = `<?xml version="1.0" encoding="utf-8" ?>
 <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
   <soap:Header>
     <t:RequestServerVersion Version="Exchange2007_SP1" />
@@ -18,17 +27,66 @@ var soapheader = `<?xml version="1.0" encoding="utf-8" ?>
   <soap:Body>
 `
 
-func Issue(ewsAddr string, username string, password string, body []byte) (*http.Response, error) {
-	b2 := []byte(soapheader)
+func SendMail(to []string, cc []string, topic string, content string) (*http.Response, error) {
+	// check username format
+	b, err := BuildTextEmail(UserName, to, cc, topic, []byte(content))
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("build text email failed: %s", err))
+	}
+
+	return Issue(ExchangeServerAddr, UserName, b)
+}
+
+func Issue(ewsAddr string, userName string, body []byte) (*http.Response, error) {
+	if userName == "" {
+		return nil, errors.New("empty user name, please provide valid email or format with domain\\account")
+	}
+
+	if ewsAddr == "" {
+		return nil, errors.New("empty ews address, please provide valid server address")
+	}
+
+	if AccessToken == "" {
+		return nil, errors.New("empty ews access token, please provide valid access token")
+	}
+
+	b2 := []byte(soapHeader)
 	b2 = append(b2, body...)
 	b2 = append(b2, "\n  </soap:Body>\n</soap:Envelope>"...)
 	req, err := http.NewRequest("POST", ewsAddr, bytes.NewReader(b2))
 	if err != nil {
-		return nil, err
+		return nil, errors.New(fmt.Sprintf("create request failed: %s", err))
 	}
-	req.SetBasicAuth(username, password)
+
+	var client *http.Client
+	re := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	isMail := re.MatchString(userName)
+	bearer := "Bearer " + AccessToken
+	if !isMail {
+		// use domain
+		l := strings.Split(userName, "\\")
+		if len(l) < 2 {
+			return nil, errors.New("wrong format of username, not email or format with domain\\account")
+		}
+
+		domain := l[0]
+		account := l[1]
+		client = &http.Client{
+			Transport: &httpNtlm.NtlmTransport{
+				Domain:          domain,
+				User:            account,
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			},
+		}
+	} else {
+		client = &http.Client{
+			Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
+		}
+	}
+	req.Header.Add("Authorization", bearer)
 	req.Header.Set("Content-Type", "text/xml")
-	client := new(http.Client)
+	second := time.Second
+	client.Timeout = 10 * second
 	client.CheckRedirect = func(req *http.Request, via []*http.Request) error { return http.ErrUseLastResponse }
 	return client.Do(req)
 }
